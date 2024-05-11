729decc175e5369d639c913b34c86c867632c2d3
myNsen <119949134+myNsen@users.noreply.github.com>
2024-05-11 16:00:08 -0400

    (Appended the details.jsx)

---
+++ movie-wormz-ai/src/components/Details.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "../styles/App.css";
+
+function Details() {
+    return(
+
+        <div class="movie-details">
+            <h1>Movie Title</h1>
+            <p>Movie Description</p>
+            <p>Movie Rating</p>
+            <p>Movie Release Date</p>
+            <p>Movie Genre</p>
+            <p>Movie Cast</p>
+            <p>Movie Box Office</p>
+            <p>Movie Language</p>
+            <p>Movie Awards</p>
+        </div>
+    );
+}
+export default Details;
