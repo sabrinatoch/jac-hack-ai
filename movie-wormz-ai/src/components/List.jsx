01c8ff5a295ac386c6d108740c5634da33c76970
Julia Forbes <forbes.juliam@gmail.com>
2024-05-11 18:04:59 -0400

    movies display

--- movie-wormz-ai/src/components/List.jsx
+++ movie-wormz-ai/src/components/List.jsx
@@ -4,15 +4,21 @@ export default function List({ movies, onMovieClick }) {
   return (
     <div id="container">
       <div className="App-list">
+        
         {movies.length > 0 ? (
           movies.map((mov) => (
-            <div
+            mov.map((m)=>(
+              <div
               className="mov"
-              key={mov.Key}
-              onClick={() => onMovieClick(mov.Key)}
+              
+              key={m.Key}
+              onClick={() => onMovieClick(m.Key)}
             >
-              <p className="title">{mov.title}</p>
+              {console.log(m)}
+              <p className="title">{m.title}</p>
             </div>
+            ))
+            
           ))
         ) : (
           <div className="none">No movies found.</div>
