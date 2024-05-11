d55509ce2b36c21295871aedf6f53c0f67c87c45
myNsen <119949134+myNsen@users.noreply.github.com>
2024-05-11 18:18:17 -0400

    (Did and undone something in the server)

--- server/server.js
+++ server/server.js
@@ -4,12 +4,6 @@ const app = express();
 const port = 8888;
 const moviedb = new MovieDb('39b66e8f6eab8dade70c8ec1356a75c3');
 
-// moviedb.searchMovie({ query: 'Alien Island' })
-// .then((res) => {
-//   console.log(res)
-// })
-// .catch(console.error);
-
 app.use(express.static(__dirname + "../movie-wormz-ai/public"));
 
 app.get("/movies/:title", async (req, res) => {
