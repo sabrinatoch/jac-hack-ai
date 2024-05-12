61e02bb8c6a89ac5e3e8aaaebfabbdb523b66673
Julia Forbes <forbes.juliam@gmail.com>
2024-05-12 05:06:33 -0400

    Update List.jsx

--- movie-wormz-ai/src/components/List.jsx
+++ movie-wormz-ai/src/components/List.jsx
@@ -66,8 +66,8 @@ export default function List({ movies, onMovieClick }) {
               ) : (
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 300" onClick={() => toggleDetails(mov.id)} >
                 <path d="M10 10 L190 10 Q200 10, 200 20 L200 280 Q200 290, 190 290 L10 290 Q0 290, 0 280 L0 20 Q0 10, 10 10" fill="#fbefbe" stroke="#000000" stroke-width="2"/>
-                <text x="100" y="40" text-anchor="middle" font-family="Arial" font-size="20" fontWeight="bold" fill="black" textLength="160" lengthAdjust="spacingAndGlyphs">{mov.title}</text>
-                <text x="100" y="55" text-anchor="middle" font-family="Arial" font-size="14" fill="#000000">{mov.release_date}</text>
+                <text x="100" y="40" textAnchor="middle" fontFamily="Arial" fontSize="20" fontWeight="bold" fill="black" textLength="160" lengthAdjust="spacingAndGlyphs">{mov.title}</text>
+                <text x="100" y="55" textAnchor="middle" fontFamily="Arial" fontSize="14" fill="#000000">{mov.release_date}</text>
                 <g transform="translate(20,70)" text-align="center">
                   <image width="141.75" href={image} align-content="bottom"/>
                 </g>
