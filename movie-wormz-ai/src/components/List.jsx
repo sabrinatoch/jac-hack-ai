dd4453ad4076fee416f8b1823318202aca52ae36
myNsen <119949134+myNsen@users.noreply.github.com>
2024-05-11 21:44:09 -0400

    (Finally added the details for a movie)

--- movie-wormz-ai/src/components/List.jsx
+++ movie-wormz-ai/src/components/List.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import Details from "./Details";
 
 export default function List({ movies, onMovieClick }) {
+  const [selectedMovieId, setSelectedMovieId] = React.useState(null);
+
+  const toggleDetails = (id) => {
+    if(selectedMovieId === id) {
+      setSelectedMovieId(null);}
+      else{
+        setSelectedMovieId(id);
+      }
+    };
   return (
     <div id="container">
       <div className="App-list">
@@ -13,8 +23,12 @@ export default function List({ movies, onMovieClick }) {
                 onClick={() => onMovieClick(mov.id)}
               >
                 {console.log(mov)}
-                <p className="title">{mov.title}</p>
-                <img src={`https://image.tmdb.org/t/p/w500/${mov.poster_path}`} width="81" height="120"/>
+                <p className="title" onClick={() => toggleDetails(mov.id)}>{mov.title}</p>
+                <img src={`https://image.tmdb.org/t/p/w500/${mov.poster_path}? `} width="81" height="120" />
+                {selectedMovieId === mov.id && <Details mov={mov} />}
+                {/* <div className="details">
+                  
+                </div> */}
               </div>
             ))
           )
